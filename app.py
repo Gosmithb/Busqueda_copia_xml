c09f9f15372d92eb2ec7b186ba4020fd82bb22c7
Gosmithb <bryangodinez00@gmail.com>
2025-09-08 13:10:59 -0700

    Correccion de validaciones, manejo de 4.0 y 3.3 de cfdi

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ import bisect
 import sqlite3
 import xml.etree.ElementTree as ET
 
+import psycopg2
+
 def seleccionar_ruta():
     root = tk.Tk()
     root.withdraw()  # Oculta la ventana principal de Tkinter
@@ -34,8 +36,8 @@ herramienta_elegida = input("""
         (1) Eliminar archivos en origen que coincidan por nombre con destino (De DESTINO toman los nombres y de ORIGEN elimina los que coincidan)
         (2) Copiar archivos de origen a destino, creando estructura de carpetas si no existen, basado en lista txt
         (3) Copiar archivos de origen a destino, creando estructura de carpetas si no existen, filtrado por .xml
-        (4) Insertar o descargar XML a DB SQLite
-        Seleccione una opción (1-3): 
+        (4) Insertar o descargar XML a DB postgresql
+        Seleccione una opción (1-4): 
     """)
 
 # ##################################################################
@@ -77,27 +79,27 @@ elif herramienta_elegida == "2":
 
     if origen and destino and txt_file:
         print(f"Realizando operaciones en: {destino}")
-
-    contador = 0
-    for root, _, files in os.walk(origen):
-        files.sort()
+        contador = 0
         nombres.sort()
 
-        for nombre in nombres:
-            index = bisect.bisect_left(files, nombre[0].upper().strip())
+        for root, _, files in os.walk(origen):
+            files.sort()
 
-        if index < len(files) and os.path.splitext(files[index])[0].upper().strip() == nombre[0].upper().strip():
-            origen_path = Path(root) / files[index]
-            destino_path = Path(destino) / files[index]
+            for nombre in nombres:
+                index = bisect.bisect_left(files, nombre[0].upper().strip())
 
-            try:
-                destino_path.parent.mkdir(parents=True, exist_ok=True)
-                shutil.copy2(origen_path, destino_path)
-            
-                print(f"{contador}: Copiado de {origen_path} a {destino_path}")
-                contador += 1
-            except Exception as e:
-                print(f"Error copiando {origen}: {e}")
+                if index < len(files) and os.path.splitext(files[index])[0].upper().strip() == nombre[0].upper().strip():
+                    origen_path = Path(root) / files[index]
+                    destino_path = Path(destino) / files[index]
+
+                    try:
+                        destino_path.parent.mkdir(parents=True, exist_ok=True)
+                        shutil.copy2(origen_path, destino_path)
+
+                        print(f"{contador}: Copiado de {origen_path} a {destino_path}")
+                        contador += 1
+                    except Exception as e:
+                        print(f"Error copiando {origen_path}: {e}")
 
 ####################################################################
 # Copiar archivos de origen a destino, creando carpetas si no existen filtrado por .xml
@@ -132,19 +134,25 @@ elif herramienta_elegida == "3":
                 except Exception as e:
                     print(f"Error copiando {origen}: {e}")
 
-##########################################################
+####################################################################
+# Insertar o descargar XML a DB SQLite
 elif herramienta_elegida == "4":
-# Insertar xml a base de datos
 
-    cfdis_nominas_db_ruta = r"C:\Users\flaco\OneDrive\Documentos\DB\Nominas"
-    conn = sqlite3.connect(cfdis_nominas_db_ruta)
-    cursor = conn.cursor()
+    conn = psycopg2.connect(
+        host="host",
+        database="bd",
+        user="user",
+        password="password",
+        port="port"
+    )
+
+    nombre_tabla = "nominas_2020.cfdi_nominas"
     errores_log = "Errores_log.txt"
-    nombre_tabla = input("Nombre de la tabla a utilizar (ejemplo: CFDIs_2024_concentrado): ")
+    cursor = conn.cursor()
 
-    def parse_fecha_iso(fecha_str: str) -> str:
+    def parse_fecha_iso(fecha_str: str):
         if not fecha_str:
-            return ''
+            return None
         
         formatos = [
             "%Y-%m-%d",          # solo fecha
@@ -155,7 +163,7 @@ elif herramienta_elegida == "4":
                 return datetime.strptime(fecha_str, fmt).date().isoformat()
             except ValueError:
                 continue
-        return ''
+        return None
 
 
     opcion = input("(1) Insertar XML a BD\n(2) Descargar XML de BD desde lista txt\nSeleccione una opción (1 o 2): ")
@@ -170,21 +178,28 @@ elif herramienta_elegida == "4":
                         ruta_archivo = Path(rootPath) / file
                         try:
                             # Leer contenido xml para parsear a txt
-                            with open(ruta_archivo, "r", encoding="utf-8") as file:
+                            with open(ruta_archivo, "r", encoding="utf-8-sig") as file:
                                 xml_content = file.read()
                             
                             tree = ET.parse(ruta_archivo)
                             root = tree.getroot()
 
                             ns = {
-                                'cfdi': 'http://www.sat.gob.mx/cfd/4',
+                                'cfdi': '',
                                 'tfd': 'http://www.sat.gob.mx/TimbreFiscalDigital',
                                 'nomina12': 'http://www.sat.gob.mx/nomina12'
                             }
 
+                            # Determinar version del cfdi
+                            version = root.attrib.get("Version")
+                            if version == "4.0":
+                                ns['cfdi'] = "http://www.sat.gob.mx/cfd/4"
+                            elif version == "3.3":
+                                ns['cfdi'] = "http://www.sat.gob.mx/cfd/3"
+
                             receptor_elem = root.find('.//cfdi:Receptor', ns)
                             nomina_elem = root.find('.//nomina12:Nomina', ns)
-                            nomina_receptor_elem = root.find('.//nomina12:Receptor', ns)
+                            nomina_receptor_elem = nomina_elem.find('.//nomina12:Receptor', ns) if nomina_elem is not None else None
                             timbre_fiscal_digital_elem = root.find('.//tfd:TimbreFiscalDigital', ns)
                             entidad_sncf_elem = nomina_elem.find('.//nomina12:EntidadSNCF', ns) if nomina_elem is not None else None
                             nomina_deducciones_elem = nomina_elem.find('.//nomina12:Deducciones', ns) if nomina_elem is not None else None
@@ -201,15 +216,15 @@ elif herramienta_elegida == "4":
 
                             # Datos receptor
                             rfc_receptor = receptor_elem.attrib.get('Rfc', '')
-                            domicilio_fiscal = receptor_elem.attrib.get('DomicilioFiscalReceptor', '')
+                            domicilio_fiscal = receptor_elem.attrib.get('DomicilioFiscalReceptor', '') or None
 
                             # Datos nomina
                             fecha_pago = nomina_elem.attrib.get('FechaPago') or ''
                             total_impuestos_retenidos = float(nomina_deducciones_elem.attrib.get('TotalImpuestosRetenidos', '0.00')) if nomina_deducciones_elem is not None else 0.00
-                            origen_recurso = entidad_sncf_elem.attrib.get('OrigenRecurso', '') if entidad_sncf_elem is not None else ''
+                            origen_recurso = entidad_sncf_elem.attrib.get('OrigenRecurso', '') if entidad_sncf_elem is not None else None
                             tipo_nomina = nomina_elem.attrib.get('TipoNomina', '')
                             total_deducciones = float(nomina_elem.attrib.get('TotalDeducciones', '0.00') or '0.00')
-                            nomina_elem.attrib.get('Puesto', '')
+                            numero_plaza = nomina_receptor_elem.attrib.get('Puesto', '') if nomina_receptor_elem is not None else None
 
                             # Datos timbre fiscal digital
                             uuid = timbre_fiscal_digital_elem.attrib.get('UUID', '')
@@ -220,38 +235,40 @@ elif herramienta_elegida == "4":
                                     llave, 
                                     domicilio_fiscal_receptor,
                                     rfc,
+                                    numero_plaza,
                                     origen_recurso,
                                     tipo_nomina,
                                     uuid,
-                                    total_deducciones,
-                                    total_impuestos_retenidos,
                                     fecha,
                                     fecha_pago,
                                     fecha_timbrado,
+                                    total_deducciones,
+                                    total_impuestos_retenidos,
                                     sub_total,
                                     total,
-                                    xml_content,
-                                    numero_plaza
-                                ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                                    xml_content
+                                ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                                ON CONFLICT (uuid) DO NOTHING
                                 """, (
                                     llave, 
                                     domicilio_fiscal, 
                                     rfc_receptor, 
+                                    numero_plaza,
                                     origen_recurso, 
                                     tipo_nomina,
                                     uuid, 
-                                    total_deducciones,
-                                    total_impuestos_retenidos, 
                                     parse_fecha_iso(fecha), 
                                     parse_fecha_iso(fecha_pago), 
                                     parse_fecha_iso(fecha_timbrado), 
+                                    total_deducciones,
+                                    total_impuestos_retenidos, 
                                     sub_total, 
                                     total, 
-                                    xml_content,
-                                    nomina_receptor_elem
-                            ))
+                                    xml_content #Se guarda como tipo xml (TEXT en sqlite, XML en postgres)
+                            )) 
                             
                             print(f"Insertado: {ruta_archivo} en {nombre_tabla}")
+                            conn.commit()
                         except Exception as e:
                             mensaje_error = f"Error al procesar {ruta_archivo}: {e}\n"
                             print(f"Error al procesar {ruta_archivo}: {e}")
@@ -263,19 +280,27 @@ elif herramienta_elegida == "4":
     def DescargarXMLdeBD():
         txt_file = seleccionar_ruta_txt()
         destino = seleccionar_ruta()
+
         with open(txt_file, "r", encoding="utf-8") as f:
             nombres = [line.strip() for line in f if line.strip()]
 
         Path(destino).mkdir(parents=True, exist_ok=True)
         for nombre in nombres:
-            cursor.execute(f"SELECT xml_content FROM {nombre_tabla} WHERE llave = ? OR uuid = ?", (nombre, nombre))
+            cursor.execute(f"SELECT xml_content FROM {nombre_tabla} WHERE llave = %s OR uuid = %s", (nombre, nombre))
             row = cursor.fetchone()
             if row is None: 
                 print(f"No se encontró XML para: {nombre}")
                 continue
+                
+            xml_content = row[0]
+
+            # Eliminar BOM si existe
+            if xml_content.startswith("\ufeff"):
+                xml_content = xml_content.lstrip("\ufeff")
 
             with open(f"{destino}/{nombre}.xml", "w", encoding="utf-8") as xml_file:
-                xml_file.write(row[0])
+                xml_file.write(xml_content)
+            
             print(f"Descargado: {destino}/{nombre}.xml")
 
     if opcion == "1":
